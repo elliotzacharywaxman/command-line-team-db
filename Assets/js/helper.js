e2099c1441df6d6fbbf7b1dc1a0459935f1281ef
elliotzacharywaxman <elliot@climatedao.xyz>
2022-06-14 18:52:55 -0400

    adding adding functionality

--- Assets/js/helper.js
+++ Assets/js/helper.js
@@ -1,7 +1,6 @@
 // Import and require mysql2
 const mysql = require('mysql2');
-const { viewPrompts } = require('./index');
-
+const inquirer = require('inquirer');
 
 const PORT = process.env.PORT || 3001;
 
@@ -16,39 +15,78 @@ const db = mysql.createConnection(
   console.log(`Connected to the staff_db database.`)
 );
 
-function viewAllDep() {
-  db.query(`SELECT * FROM departments;`, (err, result) => {
-    if (err) {
-      console.log(err);
-    }
-    console.log(result)
-    viewPrompts()
+// Viewing all departments
+function viewAllDep(cb) {
+  return db.query(`SELECT * FROM departments;`, (err, result) => {
+    console.table(result)
+    cb(result)
   });
 }
 
-function viewAllRoles() {
-  db.query(`SELECT * FROM staff_roles;`, (err, result) => {
-    if (err) {
-      console.log(err);
-    }
-    console.log(result)
-    viewPrompts()
+// Viewing all roles
+function viewAllRoles(cb) {
+  return db.query(`SELECT * FROM staff_roles;`, (err, result) => {
+    cb(result)
   });
 }
 
-function viewAllEmps() {
-  db.query(`SELECT * FROM employees;`, (err, result) => {
-    if (err) {
-      console.log(err);
-    }
-    console.log(result)
-    viewPrompts()
+// Viewing all Employees
+function viewAllEmps(cb) {
+  return db.query(`SELECT * FROM employees;`, (err, result) => {
+    cb(result)
   });
 }
 
+// Add a department
+async function addDep(cb) {
+  userInput()
+  const dep = await db.query(`INSERT INTO departments (dep_name) VALUES ("Ethics");`)
+    return db.query(`SELECT * FROM departments;`, (err, result) => {
+      cb(result)
+    });
+}
+
+// Add a role
+function addRoles(cb) {
+  return db.query(`SELECT * FROM staff_roles;`, (err, result) => {
+    cb(result)
+  });
+}
+
+// Add an employee 
+function addEmps(cb) {
+  return db.query(`SELECT * FROM employees;`, (err, result) => {
+    cb(result)
+  });
+}
+
+// Update an employee role
+function updateEmps(cb) {
+  return db.query(`SELECT * FROM employees;`, (err, result) => {
+    cb(result)
+  });
+}
+
+function userInput(){
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        message: 'What would you like to add?',
+        name: 'userInput',
+      }
+    ])
+    .then((input) => {
+      console.log(input)
+    });
+}
 
 module.exports = {
   viewAllDep,
   viewAllRoles,
-  viewAllEmps
+  viewAllEmps,
+  addDep,
+  addRoles,
+  addEmps,
+  updateEmps
 }
--- Assets/js/index.js
+++ Assets/js/index.js
@@ -1,7 +1,16 @@
 const inquirer = require('inquirer');
+const helper = require('./helper');
 const mysql = require('mysql2');
-// const helper = require("./helper");
-helper = require('./helper');
+
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: 'root',
+    password: 'dontcare',
+    database: 'staff_db'
+  },
+  console.log(`Connected to the staff_db database.`)
+);
 
 function viewPrompts() {
   inquirer
@@ -16,19 +25,132 @@ function viewPrompts() {
     .then((choice) => {
       switch(choice.stack) {
         case "View all departments":
-          viewAllDep();
+          viewDepartments();
           break;
         case "View all roles":
-          viewAllRoles();
+          viewRoles();
           break;
         case "View all employees":
-          viewAllEmps();
+          viewEmps();
+          break;
+        case "Add a department":
+          addDepartment();
+          break;
+        case "Add a role":
+          addRoles();
+          break;
+        case "Add an employee":
+          addEmps();
+          break;
+        case "Update an employee role":
+          updateEmps();
           break;
         case "Quit":
           console.log("Goodbye")
           process.exit();
       }
     });
+}
+
+function viewDepartments(){
+  console.log("viewDepartments()");
+  helper.viewAllDep((result)=>{
+    console.table(result)
+    viewPrompts();
+  })
+}
+
+function viewRoles(){
+  console.log("viewRoles()");
+  helper.viewAllRoles((result)=>{
+    console.table(result)
+    viewPrompts();
+  })
+}
+
+function viewEmps(){
+  console.log("viewEmps()");
+  helper.viewAllEmps((result)=>{
+    console.table(result)
+    viewPrompts();
+  })
+}
+
+// function addDepartment(){
+//   console.log("viewDepartments()");
+//   helper.addDep((result)=>{
+//     console.table(result)
+//     viewPrompts();
+//   })
+// }
+
+// function addRoles(){
+//   console.log("viewRoles()");
+//   helper.addRoles((result)=>{
+//     console.table(result)
+//     viewPrompts();
+//   })
+// }
+
+// function addEmps(){
+//   console.log("viewEmps()");
+//   helper.addEmps((result)=>{
+//     console.table(result)
+//     viewPrompts();
+//   })
+// }
+
+// function updateEmps(){
+//   console.log("viewEmps()");
+//   helper.updateEmps((result)=>{
+//     console.table(result)
+//     viewPrompts();
+//   })
+// }
+
+const addDepartment =() => {
+  inquirer
+    .prompt([
+      {
+        type:"input",
+        message:"What is the new department name?",
+        name: "department"
+
+      }
+    ])
+    .then((data) => {
+      const {department} = data;
+      db.query(`INSERT INTO departments (dep_name) VALUES ("${department}")`),
+        (err) => {
+          if (err) throw err;
+          console.log(`congrats! you have added ${department}!`)
+        }
+      viewPrompts();
+    })
+};
+
+function addRoles(){
+  console.log("viewRoles()");
+  helper.addRoles((result)=>{
+    console.table(result)
+    viewPrompts();
+  })
+}
+
+function addEmps(){
+  console.log("viewEmps()");
+  helper.addEmps((result)=>{
+    console.table(result)
+    viewPrompts();
+  })
+}
+
+function updateEmps(){
+  console.log("viewEmps()");
+  helper.updateEmps((result)=>{
+    console.table(result)
+    viewPrompts();
+  })
 }
 
 viewPrompts();
